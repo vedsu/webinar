9c395310557a8c7ec185e5cf636320b613d2631b
vedsu <90701239+vedsu@users.noreply.github.com>
2023-11-06 02:59:00 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './Component/Navbar.js'
+import Navbar from './Component/Navbar.js';
+import Sidebar from './Component/Sidebar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Sidebar/>
     </div>
   );
 }
