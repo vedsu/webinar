1b5a84661e100e366dfe1f799a48cd99a8d74b45
vedsu <90701239+vedsu@users.noreply.github.com>
2023-11-08 03:42:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import './Component/Navbar.js'
 import Navbar from './Component/Navbar.js';
 import Sidebar from './Component/Sidebar.js';
+import Speaker from './Component/MainPage/Speaker.js';
+import Webinar from './Component/MainPage/Webinar.js';
+import './vendor/fontawesome-free/css/all.min.css';
+import './vendor/bootstrap/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Sidebar/>
+      <Speaker/>
+      <Webinar/>
     </div>
   );
 }
